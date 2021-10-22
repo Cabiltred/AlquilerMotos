43205253b70b152a362dd255330a1f013d214104
Cabiltred <cabiltred@gmail.com>
2021-10-22 16:41:07 -0500

    ajustes de autenticación

--- src/main/java/com/devesoft/alquilerMotos/AlquilerMotosApplication.java
+++ src/main/java/com/devesoft/alquilerMotos/AlquilerMotosApplication.java
@@ -3,7 +3,6 @@ package com.devesoft.alquilerMotos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @EntityScan(basePackages = {"com.devesoft.alquilerMotos.model"})
 @SpringBootApplication
